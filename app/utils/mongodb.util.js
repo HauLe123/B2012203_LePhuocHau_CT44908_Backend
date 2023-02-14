da09cddfdafee2c0ff435466b13d88591ca416f8
Hau Le <leephuochau@gmail.com>
2023-02-15 00:27:31 +0700

    Sua loi

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,6 +1,6 @@
-const { MonggoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-class Mongodb {
+class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
         this.client = await MongoClient.connect(uri);
